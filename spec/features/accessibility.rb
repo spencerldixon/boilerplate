52fd81f33009ab9814a575aba57d2b541d06aa34
Spencer Dixon <spencerlloyddixon@gmail.com>
2023-12-10 17:39:00 +0000

    Fix lint errors

--- spec/features/accessibility.rb
+++ spec/features/accessibility.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "Accessibility", type: :system do
   it "is accessible", js: true do
     visit root_path
-    expect(page).to be_axe_clean.according_to("wcag2a", "wcag2aa" "wcag21aa", "best-practice")
+    expect(page).to be_axe_clean.according_to("wcag2a", "wcag2aa", "wcag21aa", "best-practice")
   end
 end
