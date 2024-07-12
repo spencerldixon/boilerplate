2f8beff22422fc0435dcc2040ef10e5ec759ac9c
Spencer Dixon <spencerlloyddixon@gmail.com>
2024-07-12 11:52:35 +0100

    Generate admin boolean migration in devise module

--- lib/boilerplate/modules/devise.rb
+++ lib/boilerplate/modules/devise.rb
@@ -1,4 +1,5 @@
 require "tty-prompt"
+require "active_support/inflector"
 
 module Devise
   def install_devise(silent: false, **)
@@ -11,8 +12,20 @@ module Devise
       rails_command("g devise:install")
 
       # Set up devise model
-      user_model = silent ? "User" : prompt.ask("What do you want to call your devise model?", default: "User")
-      rails_command("g devise #{user_model}")
+      resource = silent ? "User" : prompt.ask("What do you want to call your devise model?", default: "User")
+      rails_command("g devise #{resource}")
+
+      # Generate a migration and add admin column to it
+      migration_name = "add_admin_to_#{resource.pluralize.downcase}"
+      rails_command "g migration #{migration_name}"
+
+      inject_into_file Dir.glob("db/migrate/*_#{migration_name}.rb").first, after: "def change" do
+        "\n\t\tadd_column :#{resource.pluralize.downcase}, :admin, :boolean, default: false"
+      end
+
+      # Add a t&c validation to the resource model
+
+      # Enable confirmable and lockable
 
       if silent || prompt.yes?("What do you want to copy over boilerplate's prestyled devise views and partials?")
         # TODO Copy devise views
